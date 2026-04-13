bf62485c437d085c0381761be1466644c1794c57
Anusha <anusha.abburi@fielmann.com>
2026-04-13 15:18:43 +0200

    Add environment variable setup

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const user = process.env.USER_NAME;
+const db = process.env.DB_URL;
+
+console.log("User:", user);
+console.log("DB URL:", db);
+
 app.get('/', (req, res) => {
-  res.send('Hello from Kubernetes App');
+  res.send('Hello from Kubernetes App ');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
