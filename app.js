61f869f19ba0a70660527f74c0a52da24c65c059
Anusha-3109 <anusha.abburi@fielmann.com>
2026-04-13 12:59:31 +0200

    Refactor app.js to use environment variable for port

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Kubernetes App 🚀');
+  res.send('Hello from Kubernetes App');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
